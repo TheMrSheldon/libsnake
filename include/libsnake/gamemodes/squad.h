41996550dc077e3933723a051f2f579ad647a465
Tim Hagen <timhagenmail@googlemail.com>
2022-01-09 13:24:06 +0100

    implemented the challenge gamemode

--- include/libsnake/gamemodes/squad.h
+++ include/libsnake/gamemodes/squad.h
@@ -6,6 +6,11 @@
 namespace ls::gm {
 
 	class SquadGamemode final : public ls::Gamemode {
+	private:
+		bool allowBodyCollision;
+		bool sharedElimination;
+		bool sharedHealth;
+		bool sharedLength;
 	public:
 		DLLEXPORT SquadGamemode() noexcept;
 		DLLEXPORT bool isGameOver(const State& state) const noexcept override;
@@ -13,6 +18,15 @@ namespace ls::gm {
 		DLLEXPORT State stepState(const State& state, const std::vector<Move>& moves) const noexcept override;
 
 		DLLEXPORT Move getUnblockedActions(const State& state, std::size_t snakeIdx) const noexcept override;
+
+		void setAllowBodyCollisions(bool value) noexcept { this->allowBodyCollision = value; }
+		bool getAllowBodyCollisions() const noexcept { return this->allowBodyCollision; }
+		void setSharedElimination(bool value) noexcept { this->sharedElimination = value; }
+		bool getSharedElimination() const noexcept { return this->sharedElimination; }
+		void setSharedHealth(bool value) noexcept { this->sharedHealth = value; }
+		bool getSharedHealth() const noexcept { return this->sharedHealth; }
+		void setSharedLength(bool value) noexcept { this->sharedLength = value; }
+		bool getSharedLength() const noexcept { return this->sharedLength; }
 	};
 
 	/**
