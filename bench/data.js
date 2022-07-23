39e3ec0cda7471febe4459dd4f125f45a858e705
github-action-benchmark <github@users.noreply.github.com>
2022-07-23 19:33:15 +0000

    add Catch2 Benchmark (catch2) benchmark result for 700cc8dd11a699a4fbeddbeee2ea24f6dc25274b

---
+++ bench/data.js
@@ -0,0 +1,35 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1658604795337,
+  "repoUrl": "https://github.com/TheMrSheldon/libsnake",
+  "entries": {
+    "Catch2 Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "TheMrSheldon",
+            "username": "TheMrSheldon"
+          },
+          "committer": {
+            "name": "TheMrSheldon",
+            "username": "TheMrSheldon"
+          },
+          "id": "700cc8dd11a699a4fbeddbeee2ea24f6dc25274b",
+          "message": "Qualityoflife",
+          "timestamp": "2022-01-05T20:06:39Z",
+          "url": "https://github.com/TheMrSheldon/libsnake/pull/4/commits/700cc8dd11a699a4fbeddbeee2ea24f6dc25274b"
+        },
+        "date": 1658604794913,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Progression",
+            "value": 3.29716,
+            "range": "± 25.1416",
+            "unit": "us",
+            "extra": "100 samples\n9 iterations"
+          }
+        ]
+      }
+    ]
+  }
+}
