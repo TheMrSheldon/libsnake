9728261f349f07bd4ca331e47426d7c968453a12
github-action-benchmark <github@users.noreply.github.com>
2022-07-24 09:41:02 +0000

    add Catch2 Benchmark (catch2) benchmark result for f75e7bef26d083da7cf05ff6f4c651c738212512

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1658654817448,
+  "lastUpdate": 1658655662740,
   "repoUrl": "https://github.com/TheMrSheldon/libsnake",
   "entries": {
     "Catch2 Benchmark": [
@@ -133,6 +133,37 @@ window.BENCHMARK_DATA = {
             "name": "Progression",
             "value": 4.2763,
             "range": "± 1.00168",
+            "unit": "us",
+            "extra": "100 samples\n7 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "timhagenmail@googlemail.com",
+            "name": "Sheldon",
+            "username": "TheMrSheldon"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f75e7bef26d083da7cf05ff6f4c651c738212512",
+          "message": "Qualityoflife (#4)\n\n* added clang format rules\r\n\r\n* added benchmarks\r\n\r\n* refactored benchmark and added doxygen workflow\r\n\r\n* updated doxygen workflow\r\n\r\n* temporarily always run doxygen workflow\r\n\r\n* moved doxygen publish\r\n\r\n* fixed indentation\r\n\r\n* removed condition for publishing docs\r\n\r\n* publish docs now builds as well\r\n\r\n* install doxygen\r\n\r\n* installing doxygen as root\r\n\r\n* added documentation for the first steps\r\n\r\n* moved contributing to .github\r\n\r\n* updated cmakelists.txt\r\n\r\n* added automatic publishing of benchmarks\r\n\r\n* fixed typo in benchmark output name\r\n\r\n* different folder for benchmarks executable\r\n\r\n* now?\r\n\r\n* updated workflow for publishing documentation\r\n\r\n* removed condition for publishing docs\r\n\r\n* added installation of graphviz to the workflow\r\n\r\n* updated workflow to publish the doc/html folder\r\n\r\n* enabled jekyll for peaceiris/actions-gh-pages\r\n\r\n* split test and publish actions",
+          "timestamp": "2022-07-24T11:39:19+02:00",
+          "tree_id": "b167eb31fbb90726b64696e575f030d4092427f8",
+          "url": "https://github.com/TheMrSheldon/libsnake/commit/f75e7bef26d083da7cf05ff6f4c651c738212512"
+        },
+        "date": 1658655661907,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Progression",
+            "value": 3.59015,
+            "range": "± 76.0697",
             "unit": "us",
             "extra": "100 samples\n7 iterations"
           }
